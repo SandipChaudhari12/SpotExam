ac90f259d48d75c43f0064da03eb189989b40db9
SandipChaudhari <sandipchaudhari8819@gmail.com>
2023-02-02 15:27:49 +0530

    select commit

--- spot.java
+++ spot.java
@@ -1,15 +1,18 @@
 public class BubbleSort {  
-        public static void insertionSort(int array[]) {  
-                int n = array.length;  
-                for (int j = 1; j < n; j++) {  
-                    int key = array[j];  
-                    int i = j-1;  
-                    while ( (i > -1) && ( array [i] > key ) ) {  
-                        array [i+1] = array [i];  
-                        i--;  
+        public static void selectionSort(int[] arr){  
+                for (int i = 0; i < arr.length - 1; i++)  
+                {  
+                    int index = i;  
+                    for (int j = i + 1; j < arr.length; j++){  
+                        if (arr[j] < arr[index]){  
+                            index = j;//searching for lowest index  
+                        }  
                     }  
-                    array[i+1] = key;  
-                } 
+                    int smallerNumber = arr[index];   
+                    arr[index] = arr[i];  
+                    arr[i] = smallerNumber;  
+                }  
+            }   
 
      public static void main(String[] args) {  
                  int arr[] ={50,30,20,60,80,10};  
